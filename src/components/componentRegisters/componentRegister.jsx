b946840948f7ab3d342155a4e9d579e9c3e52f0a
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-11-13 18:28:05 -0500

    actualizar cosas

--- src/components/componentRegisters/componentRegister.jsx
+++ src/components/componentRegisters/componentRegister.jsx
@@ -20,7 +20,6 @@ const ComponentRegister = () => {
   };
 
   const isValidEmail = (email) => {
-    
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
@@ -30,16 +29,16 @@ const ComponentRegister = () => {
 
     if (!isValidEmail(input.email)) {
       setError("Ingresa un correo válido");
-     
+
       setTimeout(() => {
         setError("");
-      }, 4000); 
+      }, 4000);
     } else {
       setError("");
       setInput({
         displayName: "",
         email: "",
-        password: "", 
+        password: "",
         phoneNumber: "",
       });
 
@@ -49,67 +48,66 @@ const ComponentRegister = () => {
 
   return (
     <>
-    <div className={style.bodyContainer}>
+      <div className={style.bodyContainer}>
         <BackButton />
       </div>
-    <div className={style.container}>
-      
-      <label className={style.labelTitle}>Regístrate</label>
-      <form className={style.form}>
-        <div className={style.inputContainer}>
-          <input
-            className={style.input}
-            name="displayName"
-            type="text"
-            placeholder="Nombre Completo"
-            onChange={handleChangeInput}
-            value={input.displayName}
-            required
-          />
-        </div>
+      <div className={style.container}>
+        <label className={style.labelTitle}>Regístrate</label>
+        <form className={style.form}>
+          <div className={style.inputContainer}>
+            <input
+              className={style.input}
+              name="displayName"
+              type="text"
+              placeholder="Nombre Completo"
+              onChange={handleChangeInput}
+              value={input.displayName}
+              required
+            />
+          </div>
 
-        <div className={style.inputContainer}>
-          <input
-            className={style.input}
-            name="email"
-            type="email"
-            placeholder="Correo"
-            onChange={handleChangeInput}
-            value={input.email}
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <input
-            className={style.input}
-            name="password" 
-            type="password"
-            placeholder="Ingresa tu contraseña"
-            onChange={handleChangeInput}
-            value={input.password}
-            required
-          />
-        </div>
-        <div className={style.diverror}>
-          {error ? <label className={style.errorLabel}>{error}</label> : null}
-        </div>
+          <div className={style.inputContainer}>
+            <input
+              className={style.input}
+              name="email"
+              type="email"
+              placeholder="Correo"
+              onChange={handleChangeInput}
+              value={input.email}
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <input
+              className={style.input}
+              name="password"
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              onChange={handleChangeInput}
+              value={input.password}
+              required
+            />
+          </div>
+          <div className={style.diverror}>
+            {error ? <label className={style.errorLabel}>{error}</label> : null}
+          </div>
 
-        <div className={style.inputContainer}>
-          <input
-            className={style.input}
-            name="phoneNumber"
-            type="Number"
-            placeholder="Celular"
-            onChange={handleChangeInput}
-            value={input.phoneNumber}
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <button className={style.button} onClick={onSubmitInputs}>
-            Registrar
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className={style.inputContainer}>
+            <input
+              className={style.input}
+              name="phoneNumber"
+              type="Number"
+              placeholder="Celular"
+              onChange={handleChangeInput}
+              value={input.phoneNumber}
+            />
+          </div>
+          <div className={style.inputContainer}>
+            <button className={style.button} onClick={onSubmitInputs}>
+              Registrar
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
