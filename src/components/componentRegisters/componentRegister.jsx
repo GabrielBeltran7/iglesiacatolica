a8a908751a96b030191b0861117b3dbd96deaae0
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-11-14 02:15:56 -0500

    actualizar boton de firebase

--- src/components/componentRegisters/componentRegister.jsx
+++ src/components/componentRegisters/componentRegister.jsx
@@ -1,4 +1,10 @@
 import Swal from "sweetalert2"
+import {
+  FaSignInAlt,
+  FaUserPlus,
+  FaEnvelope,
+  FaLock,
+} from "react-icons/fa"; // Importa los iconos que necesitas
 import React, { useState } from "react";
 import style from "./componentRegister.module.css";
 import BackButton from "../backButton/backButton";
@@ -33,19 +39,6 @@ const componentRegister = () => {
   const isValidConfipassword = () => {
     return input.password === input.confipassword;
   };
-
-  const isEmailAlreadyRegistered = async (email) => {
-    const authForCheck = auth; 
-    try {
-      await createUserWithEmailAndPassword(authForCheck, email, "dummyPassword");
-      return false; 
-    } catch (error) {
-     
-      return error.code === "auth/email-already-in-use";
-    }
-  };
-
-
   const onSubmitInputs = async (event) => {
     event.preventDefault();
   
@@ -69,14 +62,14 @@ const componentRegister = () => {
       }, 4000);
     } else {
       try {
-        // Intenta crear el usuario
+        
         const userCredential = await createUserWithEmailAndPassword(
           auth,
           input.email,
           input.password
         );
       
-        // Usuario registrado con éxito
+       
         Swal.fire({
           icon: 'success',
           title: 'Usuario Registrado con Éxito.',
@@ -84,7 +77,7 @@ const componentRegister = () => {
           timer: 4500,
         });
       } catch (error) {
-        // Manejo específico para el error de correo electrónico ya registrado
+       
         if (error.code === 'auth/email-already-in-use') {
           Swal.fire({
             icon: 'error',
@@ -124,6 +117,7 @@ const componentRegister = () => {
         <label className={style.labelTitle}>Regístrate</label>
         <form className={style.form}>
           <div className={style.inputContainer}>
+          <FaEnvelope className={style.icon} />
             <input
               className={style.input}
               name="email"
@@ -134,6 +128,7 @@ const componentRegister = () => {
             />
           </div>
           <div className={style.inputContainer}>
+          <FaLock className={style.icon} />
             <input
               className={style.input}
               name="password"
@@ -145,6 +140,7 @@ const componentRegister = () => {
             />
           </div>
           <div className={style.inputContainer}>
+          <FaLock className={style.icon} />
             <input
               className={style.input}
               name="confipassword"
@@ -170,3 +166,176 @@ const componentRegister = () => {
 };
 
 export default componentRegister;
+
+// import Swal from "sweetalert2"
+// import React, { useState } from "react";
+// import style from "./componentRegister.module.css";
+// import BackButton from "../backButton/backButton";
+// import { auth } from "../../../api/firebase/FirebaseConfig/FirebaseConfig.js";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+
+// const componentRegister = () => {
+//   const [input, setInput] = useState({
+//     email: "",
+//     password: "",
+//     confipassword: ""
+//   });
+//   const [error, setError] = useState("");
+
+//   const handleChangeInput = (event) => {
+//     setInput({
+//       ...input,
+//       [event.target.name]: event.target.value,
+//     });
+//   };
+
+//   const isValidEmail = (email) => {
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return emailRegex.test(email);
+//   };
+
+//   const isValidPassword = (password) => {
+//     const passwordRegex = /.{6,}/;
+//     return passwordRegex.test(password);
+//   };
+
+//   const isValidConfipassword = () => {
+//     return input.password === input.confipassword;
+//   };
+
+//   const isEmailAlreadyRegistered = async (email) => {
+//     const authForCheck = auth; 
+//     try {
+//       await createUserWithEmailAndPassword(authForCheck, email, "dummyPassword");
+//       return false; 
+//     } catch (error) {
+     
+//       return error.code === "auth/email-already-in-use";
+//     }
+//   };
+
+
+//   const onSubmitInputs = async (event) => {
+//     event.preventDefault();
+  
+//     if (!isValidEmail(input.email)) {
+//       setError("Ingresa un correo válido");
+  
+//       setTimeout(() => {
+//         setError("");
+//       }, 4000);
+//     } else if (!isValidPassword(input.password)) {
+//       setError("Ingrese una contraseña de 6 caracteres");
+  
+//       setTimeout(() => {
+//         setError("");
+//       }, 4000);
+//     } else if (!isValidConfipassword()) {
+//       setError("Las contraseñas no coinciden");
+  
+//       setTimeout(() => {
+//         setError("");
+//       }, 4000);
+//     } else {
+//       try {
+//         // Intenta crear el usuario
+//         const userCredential = await createUserWithEmailAndPassword(
+//           auth,
+//           input.email,
+//           input.password
+//         );
+      
+//         // Usuario registrado con éxito
+//         Swal.fire({
+//           icon: 'success',
+//           title: 'Usuario Registrado con Éxito.',
+//           timerProgressBar: true,
+//           timer: 4500,
+//         });
+//       } catch (error) {
+//         // Manejo específico para el error de correo electrónico ya registrado
+//         if (error.code === 'auth/email-already-in-use') {
+//           Swal.fire({
+//             icon: 'error',
+//             title: 'Correo ya registrado',
+//             text: 'Debe iniciar sesión.',
+//             confirmButtonColor: '#d33',
+//             timer: 5000,
+//           });
+//         } else {
+//           // Otros errores
+         
+//           Swal.fire({
+//             icon: 'error',
+//             title: 'Error al registrar el usuario',
+//             text: 'Vuelva a intentarlo.',
+//             confirmButtonColor: '#d33',
+//             timer: 5000,
+//           });
+//         }
+//       }
+//       setError("");
+//       setInput({
+//         email: "",
+//         password: "",
+//         confipassword: ""
+//       });
+//     }
+//   };
+  
+
+//   return (
+//     <>
+//       <div className={style.bodyContainer}>
+//         <BackButton />
+//       </div>
+//       <div className={style.container}>
+//         <label className={style.labelTitle}>Regístrate</label>
+//         <form className={style.form}>
+//           <div className={style.inputContainer}>
+//             <input
+//               className={style.input}
+//               name="email"
+//               type="email"
+//               placeholder="Correo"
+//               onChange={handleChangeInput}
+//               value={input.email}
+//             />
+//           </div>
+//           <div className={style.inputContainer}>
+//             <input
+//               className={style.input}
+//               name="password"
+//               type="password"
+//               placeholder="Ingresa tu Contraseña"
+//               onChange={handleChangeInput}
+//               value={input.password}
+//               required
+//             />
+//           </div>
+//           <div className={style.inputContainer}>
+//             <input
+//               className={style.input}
+//               name="confipassword"
+//               type="password"
+//               placeholder="Confirma tu Contraseña"
+//               onChange={handleChangeInput}
+//               value={input.confipassword}
+//               required
+//             />
+//           </div>
+//           <div className={style.diverror}>
+//             {error && <label className={style.errorLabel}>{error}</label>}
+//           </div>
+//           <div className={style.inputContainer}>
+//             <button className={style.button} onClick={onSubmitInputs}>
+//               Registrar
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default componentRegister;
