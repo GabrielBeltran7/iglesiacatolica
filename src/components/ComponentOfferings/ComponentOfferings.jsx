baedc7d0fcb86763ec11fb4fc7edcc2db89711b3
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-11-18 20:10:17 -0500

    actualizar boton de muchas cosas2

--- src/components/ComponentOfferings/ComponentOfferings.jsx
+++ src/components/ComponentOfferings/ComponentOfferings.jsx
@@ -4,7 +4,7 @@ const ComponentOfferings = () => {
     <div className={style.offeringsbutton}>
       <label className={style.title}>Ofrendas</label>
       <a
-        href="https://api.whatsapp.com/send/?phone=573132315212&text=Hola%2C+estoy+interesado+en+los+servicios+que+ofrece+Innovación +Digital.+%C2%BFPodr%C3%ADa+proporcionarme+m%C3%A1s+informaci%C3%B3n+al+respecto%3F+Gracias"
+        href=""
         target="_blank"
       >
         <img
