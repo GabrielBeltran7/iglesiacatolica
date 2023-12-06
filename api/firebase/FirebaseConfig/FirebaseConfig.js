98418843e4ec40bff0efc51591228db01a130b62
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-12-06 16:01:16 -0500

    actualizar variables de entorno

--- api/firebase/FirebaseConfig/FirebaseConfig.js
+++ api/firebase/FirebaseConfig/FirebaseConfig.js
@@ -1,6 +1,7 @@
 
 
 
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -24,4 +25,28 @@ const  storage = getStorage(app)
 
 
 export { app, auth, db, storage,  };
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// import { getStorage  } from 'firebase/storage';
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAyIKCadGNtY37wkA_Er4wqGh0QTVQoRQk",
+//   authDomain: "hjnserc-64c72.firebaseapp.com",
+//   projectId: "hjnserc-64c72",
+//   storageBucket: "hjnserc-64c72.appspot.com",
+//   messagingSenderId: "642393736815",
+//   appId: "1:642393736815:web:0671ffeba4573a2749ef9a",
+//   measurementId: "G-NPJLQ7DQ79"
+// };
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app); 
+// const db = getFirestore(app);
+// const  storage = getStorage(app)
+
+
+// export { app, auth, db, storage,  };
 
