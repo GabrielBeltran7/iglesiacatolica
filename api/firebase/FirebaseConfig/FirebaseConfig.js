e4f6a5cd4076ae22690e523c4798256619adc3b7
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-12-06 15:22:43 -0500

    crear variables de entorno

--- api/firebase/FirebaseConfig/FirebaseConfig.js
+++ api/firebase/FirebaseConfig/FirebaseConfig.js
@@ -6,17 +6,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 import { getStorage  } from 'firebase/storage';
-import dotenv from 'dotenv';
-dotenv.config();
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAyIKCadGNtY37wkA_Er4wqGh0QTVQoRQk",
+  authDomain: "hjnserc-64c72.firebaseapp.com",
+  projectId: "hjnserc-64c72",
+  storageBucket: "hjnserc-64c72.appspot.com",
+  messagingSenderId: "642393736815",
+  appId: "1:642393736815:web:0671ffeba4573a2749ef9a",
+  measurementId: "G-NPJLQ7DQ79"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); 
